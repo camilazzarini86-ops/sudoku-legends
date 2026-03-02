54c5ac0d37ad5077970305d1d5a0963e442c8461
Camilla Lazzarini <camilazzarini86@gmail.com>
2026-03-02 15:42:32 +0100

    Fix production socket URL

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import io from "socket.io-client"
 
-const socket = io("http://localhost:3001")
+const socket = io("https://sudoku-legends.onrender.com")
 
 let currentBoard = []
 let solutionBoard = []
